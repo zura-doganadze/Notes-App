5340521805cfd0144c0f5e56670a2009c3c66819
zura-doganadze <123479528+zura-doganadze@users.noreply.github.com>
2023-07-31 22:38:03 +0400

    Notes App

---
+++ assets/js/app.js
@@ -0,0 +1,52 @@
+const notesContainer = document.querySelector(".notes-container");
+
+const createBtn = document.querySelector(".btn");
+
+let notes = document.querySelectorAll(".input-box");
+
+function showNotes() {
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+
+//Update history with each change
+function updateSorage() {
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
+// create a new box
+createBtn.addEventListener("click", ()=> {
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.className = "input-box";
+    inputBox.setAttribute("contenteditable", "true");
+    img.src = "assets/img/delete.png";
+    notesContainer.appendChild(inputBox).appendChild(img);
+})
+
+// Delete the box from the delete icon
+
+notesContainer.addEventListener("click", function(e) {
+    if(e.target.tagName === "IMG") {
+        e.target.parentElement.remove();
+        updateSorage()
+    }
+    else if(e.target.tagName === "p") {
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function() {
+                updateSorage();
+            }
+        })
+    }
+    
+})
+
+// Keyboard listener
+document.addEventListener("keydown", event => {
+    if(event.key === "Enter") {
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+
+    }
+})
